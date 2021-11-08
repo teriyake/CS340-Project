874085b6ef89336107a2f1692127fe05a9c2e591
teri <tke@brynmawr.edu>
2021-11-08 08:10:51 -0500

    populate Student array

--- src/IO.java
+++ src/IO.java
@@ -9,7 +9,19 @@ import java.nio.charset.StandardCharsets;
 
 public class IO {
 
-    public static int populateCourses(String f, Course[] courses, Conflict[] conflicts) {
+    public static int getStudents(String f) {
+        int ret = 0;
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            ret = Integer.parseInt(br.readLine().split("\\s+")[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public static int populateCourses(String f, Course[] courses, Conflict[] conflicts, Student[] students) {
 
         int s = 0;
 
@@ -24,11 +36,27 @@ public class IO {
                     if (courses[Integer.parseInt(ls[i])] != null) {
                         //System.out.println(courses[i]);
                         courses[Integer.parseInt(ls[i])].enroll(Integer.parseInt(ls[0]));
+
+                        if (students[Integer.parseInt(ls[0])] != null) {
+                            students[Integer.parseInt(ls[0])].addCourseO(courses[Integer.parseInt(ls[i])]);
+                        } else {
+                            Student newStudent = new Student(Integer.parseInt(ls[0]));
+                            students[Integer.parseInt(ls[0])] = newStudent;
+                            newStudent.addCourseO(courses[Integer.parseInt(ls[i])]);
+                        }
                         
                     } else {
                         Course c = new Course(Integer.parseInt(ls[i]), s);
                         courses[Integer.parseInt(ls[i])] = c;
                         c.enroll(Integer.parseInt(ls[0]));
+
+                        if (students[Integer.parseInt(ls[0])] != null) {
+                            students[Integer.parseInt(ls[0])].addCourseO(courses[Integer.parseInt(ls[i])]);
+                        } else {
+                            Student newStudent = new Student(Integer.parseInt(ls[0]));
+                            students[Integer.parseInt(ls[0])] = newStudent;
+                            newStudent.addCourseO(courses[Integer.parseInt(ls[i])]);
+                        }
                         
                     }
                 }
